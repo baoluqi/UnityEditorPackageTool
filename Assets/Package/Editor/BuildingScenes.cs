1c8d874b78e8cf7fe775870dd06fb793bb39575e
baoluqi <baoluqi123@163.com>
2015-05-26 16:40:27 +0800

    Signed-off-by: baoluqi <baoluqi123@163.com>

---
+++ Assets/Package/Editor/BuildingScenes.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+
+public class BuildingScenes : Editor {
+
+    [MenuItem("Tools/ScenesSetting")]
+    static void CheckSceneSetting() {
+
+        List<string> dirs = new List<string>();
+        GetDirs(Application.dataPath, ref dirs);
+        EditorBuildSettingsScene[] newSettings = new EditorBuildSettingsScene[dirs.Count];
+
+        for (int i = 0; i < newSettings.Length; i++)
+        {
+            newSettings[i] = new EditorBuildSettingsScene(dirs[i], true);
+        }
+
+        EditorBuildSettings.scenes = newSettings;
+        EditorApplication.SaveAssets();
+
+    }
+
+    private static void GetDirs(string dirPath, ref List<string> dirs) {
+
+        foreach (string path in Directory.GetFiles(dirPath))
+        {
+            if (Path.GetExtension(path) == ".unity") {
+
+                dirs.Add(path.Substring(path.IndexOf("Assets/") + 1));
+                
+            }
+ 
+        }
+
+        if (Directory.GetDirectories(dirPath).Length > 0)
+        {
+
+            foreach (string path in Directory.GetDirectories(dirPath))
+            {
+                GetDirs(path, ref dirs);
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Package/Editor/ExportAssets.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class ExportAssets : Editor {
+
+    [MenuItem("Tools/Export Assets")]
+    static void Build() {
+
+        if (Selection.objects == null) return;
+        List<string> paths = new List<string>();
+        foreach (UnityEngine.Object o in Selection.objects)
+        {
+            paths.Add(AssetDatabase.GetAssetPath(o));
+        }
+
+        AssetDatabase.ExportPackage(paths.ToArray(), "Assets/UnityPackage/" + Selection.objects[0].name + "add.unitypackage", ExportPackageOptions.IncludeDependencies);
+        AssetDatabase.Refresh();
+    }
+}
